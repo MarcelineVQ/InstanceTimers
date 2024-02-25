27491a7b629bbc4b43d2c9c191cb964347d4ce26
MarcelineVQ <marcythevq@gmail.com>
2024-02-25 01:10:04 -0800

    initial

---
+++ InstanceTimers.lua
@@ -0,0 +1,94 @@
+local function print(msg)
+    DEFAULT_CHAT_FRAME:AddMessage(msg)
+end
+
+-- local _G = getfenv(0)
+
+InstanceTimers = CreateFrame("Frame")
+
+local function tsize(table)
+  local i = 0
+  for _ in pairs(table) do i = i + 1 end
+  return i
+end
+
+-- local function sortDB()
+--   local a = {}
+--   for n in pairs(lockoutDB) do table.insert(a, n) end
+--   table.sort(a,function (x,y)
+--     print(x[3])
+--     print(y[3])
+--     return x[3] < y[3] end)
+--   lockoutDB = a
+-- end
+
+-- function pairsByKeys (t, f)
+--   local a = {}
+--   for n in pairs(t) do table.insert(a, n) end
+--   table.sort(a, f)
+--   local i = 0      -- iterator variable
+--   local iter = function ()   -- iterator function
+--     i = i + 1
+--     if a[i] == nil then return nil
+--     else return a[i], t[a[i]]
+--     end
+--   end
+--   return iter
+-- end
+
+local function compStamp(a,b)
+  local x = a[2]
+  local y = b[2]
+  return x[3] < y[3]
+end
+
+local function lookup(table,value)
+  for k,v in pairs(table) do
+    if v == value then return true end
+  end
+  return false
+end
+
+local function EventHandler()
+  if event == "ZONE_CHANGED_NEW_AREA" then
+    local zonename = GetZoneText()
+    if IsInInstance() then
+      tinsert(lockoutDB,{UnitName("player"),zonename,time()})
+    end
+  end
+end
+
+local function showTimers()
+  local now = time()
+  if tsize(lockoutDB) > 0 then
+    print("Instance lockout timers:")
+    for k,v in pairs(lockoutDB) do
+      local character,instance,started = v[1],v[2],v[3]
+      local rem = now - started
+      if rem > 3600 then
+        lockoutDB[k] = nil
+      else
+        print(character .. "'s " .. instance .. " @ " .. date("%H:%M:%S",started) .. ", wait: " .. date("%Mm%Ss",3600-rem))
+      end
+    end
+  else
+    print("You have no instance lockout timers.")
+  end
+end
+
+local function handleCommands(msg,editbox)
+  if msg == "del" or msg == "delete" or msg == "rem" or msg == "remove" then
+    tremove(lockoutDB)
+  elseif msg ~= "" then
+    print("Type /its del to remove the latest timestamp.")
+  else
+    showTimers()
+  end
+end
+
+InstanceTimers:RegisterEvent("ZONE_CHANGED_NEW_AREA")
+InstanceTimers:SetScript("OnEvent", EventHandler)
+
+SLASH_INSTANCETIMERS1 = "/instancetimers";
+SLASH_INSTANCETIMERS2 = "/its";
+SlashCmdList["INSTANCETIMERS"] = handleCommands
---
+++ readme.md
@@ -0,0 +1,18 @@
+Instance Lockout Timers
+===
+
+This addon for 1.12 vanilla wow clients tracks your 5 instance lockouts for you.  
+
+Check your current lockouts with either  
+```
+/instancetimers
+/its
+```
+
+A limitation of the 1.12 client is that you can't know if your instance entrance is fresh. If you die and have to run back inside a new timer will be formed. In this case you may remove it with the follow commands, which all do the same thing.  
+```
+/ts del
+/ts delete
+/its rem
+/its remove
+```
