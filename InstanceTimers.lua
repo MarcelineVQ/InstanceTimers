368f627ed860db315372732cf3f452a938f45ece
MarcelineVQ <marcythevq@gmail.com>
2024-04-29 16:45:02 -0700

    much cleanup

--- InstanceTimers.lua
+++ InstanceTimers.lua
@@ -45,8 +45,7 @@ local function clearExpired(time,duration)
     local started = v[3]
     local rem = time - started
     if rem < duration then tinsert(a,v) end
-    -- if rem > duration then InstanceTimersDB.db[k] = nil else tinsert(a,v) end
-  end -- ^ lockout isnt doing anything here
+  end
   InstanceTimersDB.db = a
 end
 
@@ -71,11 +70,15 @@ local default_OnUpdate = InstanceTimers:GetScript("OnUpdate")
 local function timedAnnounce()
   delay = delay + arg1
   if delay > 5 then
+
+    -- use this delay to update the zone for the last entry
+    if IsInInstance() then InstanceTimersDB.db[tsize(InstanceTimersDB.db)][2] = GetZoneText() end
+
     delay = 0
     clearExpired(time(),3600)
     local limit = 5 - tsize(InstanceTimersDB.db)
     -- if we have none locked or we just zonechanged from death why mention it
-    if limit < 5 and not UnitIsGhost("player") then
+    if InstanceTimersDB.announce and limit < 5 and not UnitIsGhost("player") then
       its_print(limit.." instance lockouts remain, oldest expires in: "
         ..date("%Mm%Ss",3600-(time() - InstanceTimersDB.db[1][3])))
     end
@@ -83,126 +86,18 @@ local function timedAnnounce()
   end
 end
 
-
-local function invalidTable()
-  -- table of when a new timer should not be made
-  -- playered entered as ghost
-  -- player logged in and their own last lockout still had a timer and they either still have a group or still don't
-
-  -- table of when a new timer should be made
-  -- 
-end
-
--- I need to account to logging into a raid instance too
--- this uses a lockout but only once, even if it's a saved it
--- I need to account for running back in from ghost state, this shouldn't make a new timer
--- some kind of 'from world' variable that if set precludes adding more
--- also if it's a saved it it shouln't count towards the limit
--- How should I handle "The Upper Necropolis"? Are there any other similar cases? Should I just keep note of the last 'zone name'?
-
--- bascially I wanna keep the name of the last zone I was in, if it's the same zone I log into and there's already
--- a timer for that zone then it doens't use a new timer
-
--- what about the case that player makes new group and runs in as ghost?
--- does Reset Instanced fire an event or do you have to scan chat? It does not, sad.
--- CHAT_MSG_SYSTEM
-local last_zone = GetZoneText()
-local was_ghost = false
-local in_group = false
-local group_dropped = false
--- local just_logged_in = false
-
--- potential zone-in
-local entering_world = false
-
--- zone changed new area or and entering_world 
--- meeting stone changed
-
 local function EventHandler()
-  -- if event == "ZONE_CHANGED_NEW_AREA" and UnitIsGhost("player") then
-    -- was_ghost = true
-  -- This DOES NOT work, unghosting happens before you enter the instance
-  -- elseif event == "PLAYER_UNGHOST" and not IsInInstance() then
-    -- was_ghost = false
-  -- elseif event == "CHAT_MSG_SYSTEM" and arg1 == "You have entered too many instances recently." then
-  --   debug_print("Over instance limit.")
-
-  -- elseif event == "PLAYER_LOGIN" then
-    -- just_logged_in = true
-  -- elseif event == "ZONE_CHANGED_NEW_AREA" and not IsInInstance() then
-  --   just_logged_in = false
-  -- this is a bust, it queries too early zone is wrong
-  -- if event == "UPDATE_INSTANCE_INFO" then
-  --   local zone_name = GetZoneText()
-  --   if IsInInstance() then
-  --     its_print("info updated inside instance")
-  --     print(zone_name)
-  --   end
-  --   if not IsInInstance() then
-  --     its_print("info updated outside instance")
-  --     print(zone_name)
-  --   end
-  -- if event == "ZONE_CHANGED_NEW_AREA" and not IsInInstance() then
-  --   if not UnitIsGhost("player") then
-  --     debug_print("not a ghost and not in an instance")
-  --     InstanceTimersDB.in_instance[UnitName("player")] = nil
-  --   end
-  --   if max(GetNumPartyMembers(),GetNumRaidMembers()) == 0 then
-  --     debug_print("zc group dropped")
-  --     group_dropped = true -- this might be an issue for single-member raid entries like flask making?
-  --   end
-  -- elseif event == "RAID_TARGET_UPDATE" then
-  --   debug_print("rt party changed")
-  --   if max(GetNumPartyMembers(),GetNumRaidMembers()) == 0 then
-  --     debug_print("rt group dropped")
-  --     group_dropped = true -- this might be an issue for single-member raid entries like flask making?
-  --   else
-  --     debug_print("not dropped")
-  --     group_dropped = false
-  --   end
-    if event == "PLAYER_ENTERING_WORLD" and IsInInstance() then
-    -- if entering_world and event == "ZONE_CHANGED_NEW_AREA" or event == "ZONE_CHANGED_INDOORS" and IsInInstance() then
+  -- keeping this dumb, events are very unreliable in 1.12, we re-update the zone info using timedAnnounce to be accurate
+  if event == "PLAYER_ENTERING_WORLD" and IsInInstance() then
     local zone_name = GetZoneText()
     local player = UnitName("player")
-    -- local had_instance = InstanceTimersDB.in_instance[player]
-    entering_world = false
+    local now = time()
 
-    -- if not group_dropped and had_instance and had_instance == zone_name then
-    --   debug_print("doing previous save check")
-    --   -- find out if player was saved here already
-    --   local tz = tsize(InstanceTimersDB.db)
-    --   for i=1,tz do
-    --     local v = InstanceTimersDB.db[tz + 1 - i]
-    --     if v[1] == player then
-    --       if v[2] == zone_name then
-    --         return nil -- last instance was this one
-    --       else
-    --         break -- last instance wasn't this one
-    --       end
-    --     end
-    --   end
-    -- end
     debug_print("adding new timer")
-    -- InstanceTimersDB.in_instance[player] = zone_name
-    tinsert(InstanceTimersDB.db,{player,zone_name,time()})
-    if InstanceTimersDB.announce then InstanceTimers:SetScript("OnUpdate", timedAnnounce) end
-  -- elseif event == "PLAYER_ENTERING_WORLD" and IsInInstance() then
-    -- entering_world = true
-    -- its_print("enterworld_wasinside")
-  -- elseif event == "PLAYER_ENTERING_WORLD" then
-  --   entering_world = true
-  --   its_print("enterworld")
+    tinsert(InstanceTimersDB.db,{player,zone_name,now})
+    -- if InstanceTimersDB.announce then InstanceTimers:SetScript("OnUpdate", timedAnnounce) end
+    InstanceTimers:SetScript("OnUpdate", timedAnnounce)
   end
-  -- elseif event == "PLAYER_LEAVING_WORLD" then
-  --   local zone_name = GetZoneText()
-  --   -- save if the player is inside an instance, and which one if so
-  --   if IsInInstance() then
-  --     debug_print(zone_name)
-  --     InstanceTimersDB.in_instance[UnitName("player")] = zone_name
-  --   -- else
-  --   --   InstanceTimersDB.in_instance[UnitName("player")] = nil
-  --   end
-  -- end
 end
 
 local function Init()
